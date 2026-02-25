f2dc340088c473bb60ce5be1a015e5d57414259c
thelfer <thelfer@users.noreply.github.com>
2026-02-25 15:35:32 +0000

    deploy: thelfer/mfem-mgis@a1f3d854ea03ee989b1f56dd15f446a629d63931

--- mfem-mgis-doxygen/search/enumvalues_10.js
+++ mfem-mgis-doxygen/search/enumvalues_10.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['tangent_0',['tangent',['../namespacemfem__mgis.html#a48e078ec8d415cbe6176da998e2d23b0a4da56c3c2d4ee59a3461da5b2ad1a25d',1,'mfem_mgis::TANGENT'],['../namespacemfem__mgis.html#a73a830d83c7c7d1101ab885739b12ea5a4da56c3c2d4ee59a3461da5b2ad1a25d',1,'mfem_mgis::TANGENT']]],
-  ['tfeleigensolver_1',['TFELEIGENSOLVER',['../structtfel_1_1math_1_1stensor__common.html#a58592091a3f12c608a233e411cbb311ea87fe3619f39fa17d92589c5e17d4fe82',1,'tfel::math::stensor_common']]],
-  ['thermodynamicforceconstraint_2',['THERMODYNAMICFORCECONSTRAINT',['../structmtest_1_1_non_linear_constraint.html#ab2fe750b8e0f5e46bf3be6665148d413a4d63c3ee829d327be2404285b80113f2',1,'mtest::NonLinearConstraint']]]
+  ['tfeleigensolver_0',['TFELEIGENSOLVER',['../structtfel_1_1math_1_1stensor__common.html#a58592091a3f12c608a233e411cbb311ea87fe3619f39fa17d92589c5e17d4fe82',1,'tfel::math::stensor_common']]],
+  ['thermodynamicforceconstraint_1',['THERMODYNAMICFORCECONSTRAINT',['../structmtest_1_1_non_linear_constraint.html#ab2fe750b8e0f5e46bf3be6665148d413a4d63c3ee829d327be2404285b80113f2',1,'mtest::NonLinearConstraint']]]
 ];
--- mfem-mgis-doxygen/search/pages_4.js
+++ mfem-mgis-doxygen/search/pages_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['list_0',['Deprecated List',['../deprecated.html',1,'']]]
+  ['mfem_20mgis_20project_0',['An introduction to the MFEM/MGIS project',['../index.html',1,'']]],
+  ['mgis_20project_1',['An introduction to the MFEM/MGIS project',['../index.html',1,'']]]
 ];

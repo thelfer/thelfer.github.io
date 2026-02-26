bffb1fc28cabd9c6590aa7f4fe90c8837a1720dc
thelfer <thelfer@users.noreply.github.com>
2026-02-26 20:31:43 +0000

    deploy: thelfer/mfem-mgis@e96c6cf4480ae228f69a0c3ba2a8361a3a08ec14

---
+++ mfem-mgis-doxygen/structmfem__mgis_1_1_prediction_policy.js
@@ -0,0 +1,6 @@
+var structmfem__mgis_1_1_prediction_policy =
+[
+    [ "integration_operator", "structmfem__mgis_1_1_prediction_policy.html#afbb90aea4eb8b950021e54834586d0e7", null ],
+    [ "null_time_increment", "structmfem__mgis_1_1_prediction_policy.html#aecb6b7968d6da8e871b0c126cd9788d9", null ],
+    [ "prediction_operator", "structmfem__mgis_1_1_prediction_policy.html#adf31dd21819229946c556c2425acc9cc", null ]
+];

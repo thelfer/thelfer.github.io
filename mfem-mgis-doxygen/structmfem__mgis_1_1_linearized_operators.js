a49fcc455a1b807f7960dc30386ba9438f095c89
thelfer <thelfer@users.noreply.github.com>
2026-02-27 09:11:32 +0000

    deploy: thelfer/mfem-mgis@cd93ed5131cbb52c23add6ca1f8ed09a52d60151

--- mfem-mgis-doxygen/structmfem__mgis_1_1_linearized_operators.js
+++ mfem-mgis-doxygen/structmfem__mgis_1_1_linearized_operators.js
@@ -1,5 +1,5 @@
 var structmfem__mgis_1_1_linearized_operators =
 [
-    [ "Fi", "structmfem__mgis_1_1_linearized_operators.html#a46dc1589e509c986f1ee15d0ed7d3ec3", null ],
-    [ "K", "structmfem__mgis_1_1_linearized_operators.html#a6b7e92a13882e2cde4a179463381963b", null ]
+    [ "K", "structmfem__mgis_1_1_linearized_operators.html#a6b7e92a13882e2cde4a179463381963b", null ],
+    [ "mFi", "structmfem__mgis_1_1_linearized_operators.html#a2fb0a5c7f448ea1d79877010e1420978", null ]
 ];

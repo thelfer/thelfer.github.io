340d81fa4d59106144a50831ad8f054595e35fbf
thelfer <thelfer@users.noreply.github.com>
2026-03-12 14:32:15 +0000

    deploy: thelfer/mfem-mgis@8772bbadd8a9009b47bf220c5d4c6f8d3aae2b99

--- mfem-mgis-doxygen/structmfem__mgis_1_1_abstract_partial_quadrature_function_evaluator.js
+++ mfem-mgis-doxygen/structmfem__mgis_1_1_abstract_partial_quadrature_function_evaluator.js
@@ -1,5 +1,5 @@
 var structmfem__mgis_1_1_abstract_partial_quadrature_function_evaluator =
 [
     [ "~AbstractPartialQuadratureFunctionEvaluator", "structmfem__mgis_1_1_abstract_partial_quadrature_function_evaluator.html#ae81562a2391b56125c390f4a85d9cd3b", null ],
-    [ "getNumberOfComponents", "structmfem__mgis_1_1_abstract_partial_quadrature_function_evaluator.html#ae93e0e5bcabf0615c89baf5c2f4f5ea7", null ]
+    [ "getNumberOfComponents", "structmfem__mgis_1_1_abstract_partial_quadrature_function_evaluator.html#a6658f9a7089fb85a1c35a092d097a8e3", null ]
 ];

79788655b576ca2b5e9129e4054cade7acadeb5e
Irvise <Irvise@users.noreply.github.com>
2026-05-15 22:01:12 +0000

    deploy: thelfer/tfel@1681a569420299cde0f6722077ded63ce0eb9e1a

--- tfel/doxygen/structmfront_1_1_material_property_interface_factory.js
+++ tfel/doxygen/structmfront_1_1_material_property_interface_factory.js
@@ -1,6 +1,5 @@
 var structmfront_1_1_material_property_interface_factory =
 [
     [ "exists", "structmfront_1_1_material_property_interface_factory.html#a1ee4b01532be64dd5a529431d35802b5", null ],
-    [ "getInterface", "structmfront_1_1_material_property_interface_factory.html#a6ea11b4488adc73a06ba33d76ef10fa6", null ],
-    [ "getUniqueNameFromAlias", "structmfront_1_1_material_property_interface_factory.html#a19df86e81be59ec3bad5ba728669d40a", null ]
+    [ "getInterface", "structmfront_1_1_material_property_interface_factory.html#a6ea11b4488adc73a06ba33d76ef10fa6", null ]
 ];
---
+++ tfel/doxygen/structmfront_1_1_open_radioss_interface.js
@@ -0,0 +1,9 @@
+var structmfront_1_1_open_radioss_interface =
+[
+    [ "~OpenRadiossInterface", "structmfront_1_1_open_radioss_interface.html#aa171c4c5775ea419b9322e61139af541", null ],
+    [ "endTreatment", "structmfront_1_1_open_radioss_interface.html#a76d526a904c39b89072b2d22aee64ed0", null ],
+    [ "getInterfaceName", "structmfront_1_1_open_radioss_interface.html#ab96248190bc63a78df22c76433401338", null ],
+    [ "getModellingHypothesesToBeTreated", "structmfront_1_1_open_radioss_interface.html#af0fcb2619632cc02995c5d00b1f75827", null ],
+    [ "getTargetsDescription", "structmfront_1_1_open_radioss_interface.html#a2acbd6de48ded3870db779de95d7451d", null ],
+    [ "treatKeyword", "structmfront_1_1_open_radioss_interface.html#a1cbbf4ccdf3020be619966a40a134254", null ]
+];

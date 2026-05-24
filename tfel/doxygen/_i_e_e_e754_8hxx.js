12eb44c2dcb39f258f98c025eedee2cf84b85882
thelfer <thelfer@users.noreply.github.com>
2026-05-24 13:50:24 +0000

    deploy: thelfer/tfel@feede129d41140189d1cbcf1d8917b88ef83f967

--- tfel/doxygen/_i_e_e_e754_8hxx.js
+++ tfel/doxygen/_i_e_e_e754_8hxx.js
@@ -5,8 +5,8 @@ var _i_e_e_e754_8hxx =
     [ "fpclassify", "_i_e_e_e754_8hxx.html#aa6f55c517e3d43b7fc2bfd3130281965", null ],
     [ "isfinite", "_i_e_e_e754_8hxx.html#a0226d7f8106bc6889f0500ab747fb766", null ],
     [ "isfinite", "_i_e_e_e754_8hxx.html#a345963abddc7a2d7e9416843396c27f7", null ],
-    [ "isfinite", "_i_e_e_e754_8hxx.html#a2a2ea7d6861359e4b6823be42e57493d", null ],
+    [ "isfinite", "_i_e_e_e754_8hxx.html#aaa194b47d870cb0c2f0aa6dbbaf8e057", null ],
     [ "isnan", "_i_e_e_e754_8hxx.html#abf50962f503ef6aaec8db07ea8bae98a", null ],
     [ "isnan", "_i_e_e_e754_8hxx.html#a3450c8ef9f16ee7b747eec1e045ec45c", null ],
-    [ "isnan", "_i_e_e_e754_8hxx.html#a6c443830aac514ce0fd5f25e7ba86931", null ]
+    [ "isnan", "_i_e_e_e754_8hxx.html#aed1781de8fc7c7fa653dadae15ec2330", null ]
 ];
--- tfel/doxygen/_i_e_e_e754_8ixx.js
+++ tfel/doxygen/_i_e_e_e754_8ixx.js
@@ -4,8 +4,8 @@ var _i_e_e_e754_8ixx =
     [ "fpclassify", "_i_e_e_e754_8ixx.html#add4cffb31c0fb5e82030fe9a3efc4063", null ],
     [ "isfinite", "_i_e_e_e754_8ixx.html#a0226d7f8106bc6889f0500ab747fb766", null ],
     [ "isfinite", "_i_e_e_e754_8ixx.html#a345963abddc7a2d7e9416843396c27f7", null ],
-    [ "isfinite", "_i_e_e_e754_8ixx.html#a2a2ea7d6861359e4b6823be42e57493d", null ],
+    [ "isfinite", "_i_e_e_e754_8ixx.html#aaa194b47d870cb0c2f0aa6dbbaf8e057", null ],
     [ "isnan", "_i_e_e_e754_8ixx.html#abf50962f503ef6aaec8db07ea8bae98a", null ],
     [ "isnan", "_i_e_e_e754_8ixx.html#a3450c8ef9f16ee7b747eec1e045ec45c", null ],
-    [ "isnan", "_i_e_e_e754_8ixx.html#a6c443830aac514ce0fd5f25e7ba86931", null ]
+    [ "isnan", "_i_e_e_e754_8ixx.html#aed1781de8fc7c7fa653dadae15ec2330", null ]
 ];

56f56f4e3685ea94da00cb30b0acd2ea08ad5f77
antoinecea <antoinecea@users.noreply.github.com>
2026-06-15 08:21:38 +0000

    deploy: thelfer/tfel@18e875d5f20191876b9481d9758b0b2334d641ec

--- tfel/doxygen/_c_u_d_a_backend_8hxx.js
+++
@@ -1,4 +0,0 @@
-var _c_u_d_a_backend_8hxx =
-[
-    [ "mfront::generic_parallel::material_property::CUDABackend", "structmfront_1_1generic__parallel_1_1material__property_1_1_c_u_d_a_backend.html", "structmfront_1_1generic__parallel_1_1material__property_1_1_c_u_d_a_backend" ]
-];
--- tfel/doxygen/structmfront_1_1_behaviour_interface_factory.js
+++ tfel/doxygen/structmfront_1_1_behaviour_interface_factory.js
@@ -1,6 +1,5 @@
 var structmfront_1_1_behaviour_interface_factory =
 [
     [ "exists", "structmfront_1_1_behaviour_interface_factory.html#a55ecf05f816d878e077dbb42863e7f94", null ],
-    [ "getInterface", "structmfront_1_1_behaviour_interface_factory.html#a6ef2d55816ad97825af1ae84766fbb2d", null ],
-    [ "getUniqueNameFromAlias", "structmfront_1_1_behaviour_interface_factory.html#a8e70f0ee43c1ad3f6ff166852750728e", null ]
+    [ "getInterface", "structmfront_1_1_behaviour_interface_factory.html#a6ef2d55816ad97825af1ae84766fbb2d", null ]
 ];

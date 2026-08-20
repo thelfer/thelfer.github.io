e85f312551750ccc60b0c6820cd09259f8ff7c66
rprat-pro <rprat-pro@users.noreply.github.com>
2026-08-20 14:29:43 +0000

    deploy: thelfer/mfem-mgis@83b2fb498e2551fdb2f65dd5c294b7ce949cad70

--- mfem-mgis-doxygen/spack_2opt_2spack_2linux-zen2_2mgis-master-oeeftvzu44dailar2fguebjrt6hue7ak_2include_2_m_g_i_s_2_function_2_algorithms_8hxx.js
+++
@@ -1,6 +0,0 @@
-var spack_2opt_2spack_2linux_zen2_2mgis_master_oeeftvzu44dailar2fguebjrt6hue7ak_2include_2_m_g_i_s_2_function_2_algorithms_8hxx =
-[
-    [ "mgis::function::internals::isEvaluatorAssignableToFunction", "conceptmgis_1_1function_1_1internals_1_1is_evaluator_assignable_to_function.html", null ],
-    [ "assign", "spack_2opt_2spack_2linux-zen2_2mgis-master-oeeftvzu44dailar2fguebjrt6hue7ak_2include_2_m_g_i_s_2_function_2_algorithms_8hxx.html#afe33d04ce956c767178fa7787bfad4e1", null ],
-    [ "scalar_reduce", "spack_2opt_2spack_2linux-zen2_2mgis-master-oeeftvzu44dailar2fguebjrt6hue7ak_2include_2_m_g_i_s_2_function_2_algorithms_8hxx.html#a932a34e7734bd624edfe1d5496376e3e", null ]
-];

91df8d69751542ad07f889dad89c6470cbc59b8e
thelfer <thelfer@users.noreply.github.com>
2026-09-18 11:22:51 +0000

    deploy: thelfer/mfem-mgis@21f7f5ffdc05ce6337e2c8af2446be77e697ed3b

--- mfem-mgis-doxygen/dir_81f243ec597fa75752a8b8d06e06430a.js
+++
@@ -1,8 +0,0 @@
-var dir_81f243ec597fa75752a8b8d06e06430a =
-[
-    [ "Construct.hxx", "_construct_8hxx.html", "_construct_8hxx" ],
-    [ "Invoke.hxx", "_invoke_8hxx.html", "_invoke_8hxx" ],
-    [ "Invoke.ixx", "_invoke_8ixx.html", null ],
-    [ "Markdown.hxx", "_markdown_8hxx.html", "_markdown_8hxx" ],
-    [ "OptionalReference.hxx", "_optional_reference_8hxx.html", "_optional_reference_8hxx" ]
-];
--- mfem-mgis-doxygen/dir_fbb0aa8515d3c58d24357d8ae304e98d.js
+++
@@ -1,4 +0,0 @@
-var dir_fbb0aa8515d3c58d24357d8ae304e98d =
-[
-    [ "Model.hxx", "_model_8hxx.html", "_model_8hxx" ]
-];
